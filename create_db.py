8b2c2b5b81d4ed41824e271daaf22b5140761ef5
janaaa <115631886+Januu23@users.noreply.github.com>
2025-05-26 16:44:13 +0300

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -1,10 +1,9 @@
 import sqlite3
 
-# Verbindet sich mit dati.db (wird erstellt, falls nicht vorhanden)
 conn = sqlite3.connect('dati.db')
 c = conn.cursor()
 
-# Erstellt Tabelle rezultati mit richtigen Feldern
+
 c.execute("""
 CREATE TABLE IF NOT EXISTS rezultati (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
